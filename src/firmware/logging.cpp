faf89c3092eaecc6b5acf08aaf40ee4af7c27aa4
Linus Vogel <44703775+linvogel@users.noreply.github.com>
2023-02-07 01:13:11 +0100

    added logging system

---
+++ src/firmware/logging.cpp
@@ -0,0 +1,68 @@
+#include "logging.hpp"
+
+#include <format>
+#include <vector>
+#include <ctime>
+#include <algorithm>
+
+static std::vector<std::ostream*> out_streams;
+static std::vector<std::ostream*> err_streams;
+
+static std::vector<std::string> level_names(128, std::string{});
+
+static dimmer::logging::log_level current_level = dimmer::logging::TRACE;
+
+void dimmer::logging::init()
+{
+	
+	out_streams.push_back(&std::cout);
+	err_streams.push_back(&std::cerr);
+	
+	level_names[dimmer::logging::FATAL] = "FATAL";
+	level_names[dimmer::logging::ERROR] = "ERROR";
+	level_names[dimmer::logging::WARNING] = "WARNING";
+	level_names[dimmer::logging::INFO] = "INFO";
+	level_names[dimmer::logging::DEBUG] = "DEBUG";
+	level_names[dimmer::logging::VERBOSE] = "VERBOSE";
+	level_names[dimmer::logging::TRACE] = "TRACE";
+}
+
+void dimmer::logging::set_log_level(dimmer::logging::log_level level)
+{
+	current_level = level;
+}
+
+void dimmer::logging::add_out_stream(std::ostream out)
+{
+	out_streams.push_back(&out);
+}
+
+void dimmer::logging::add_err_stream(std::ostream out)
+{
+	err_streams.push_back(&out);
+}
+
+void dimmer::logging::_log(std::string mod_name, dimmer::logging::log_level level, std::string &fmt, va_list args)
+{
+	if (level > current_level) return;
+	char date_time[256];
+	time_t raw_time;
+	struct tm timeinfo;
+	time(&raw_time);
+	localtime_s(&timeinfo, &raw_time);
+	strftime(date_time, 255, "%Y-%m-%d %H:%M:%S", &timeinfo);
+	
+	std::transform(mod_name.begin(), mod_name.end(), mod_name.begin(), static_cast<int (*)(int)>(&std::toupper));
+	std::string msg = std::format(fmt, args);
+	std::string prefix = std::format("[{}][{}][{}] ", date_time, mod_name, level_names[level]);
+	
+	for (int i = 0; i < out_streams.size(); i++) *out_streams[i] << prefix << msg << std::endl;
+	if (level <= dimmer::logging::WARNING) for (int i = 1; i < err_streams.size(); i++) *err_streams[i] << prefix << msg << std::endl;
+}
+
+bool dimmer::logging::register_log_level(int level, std::string name)
+{
+	if (level_names[level].length() > 0) return false;
+	level_names[level] = name;
+	return true;
+}
---
+++ src/firmware/logging.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <varargs.h>
+
+#define fatal(...) dimmer::logging::_fatal(__VA_ARGS__)
+#define error(...) dimmer::logging::_error(__VA_ARGS__)
+#define warn(...) dimmer::logging::_warn(__VA_ARGS__)
+#define info(...) dimmer::logging::_info(__VA_ARGS__)
+#define debug(...) dimmer::logging::_debug(__VA_ARGS__)
+#define verbose(...) dimmer::logging::_verbose(__VA_ARGS__)
+#define trace(...) dimmer::logging::_trace(__VA_ARGS__)
+
+namespace dimmer
+{
+	namespace logging {
+		enum log_level {
+			FATAL=0,
+			ERROR=10,
+			WARNING=20,
+			INFO=30,
+			DEBUG=40,
+			VERBOSE=50,
+			TRACE=60
+		};
+		
+		void init();
+		void set_log_level(log_level level);
+		void add_out_stream(std::ostream out);
+		void add_err_stream(std::ostream out);
+		bool register_log_level(int priority, std::string name);
+		void _log(std::string mod_name, log_level level, std::string &format, va_list args);
+		
+		
+		inline void log(log_level level, std::string &format, va_list args) { _log(_DIMMER_FILE_NAME_, level, format, args); }
+		
+		inline void _fatal(std::string msg, ...) { va_list args; __va_start(&args, msg); log(log_level::FATAL, msg, args); }
+		inline void _error(std::string msg, ...) { va_list args; __va_start(&args, msg); log(log_level::ERROR, msg, args); }
+		inline void _warn(std::string msg, ...) { va_list args; __va_start(&args, msg); log(log_level::WARNING, msg, args); }
+		inline void _info(std::string msg, ...) { va_list args; __va_start(&args, msg); log(log_level::INFO, msg, args); }
+		inline void _debug(std::string msg, ...) { va_list args; __va_start(&args, msg); log(log_level::DEBUG, msg, args); }
+		inline void _verbose(std::string msg, ...) { va_list args; __va_start(&args, msg); log(log_level::VERBOSE, msg, args); }
+		inline void _trace(std::string msg, ...) { va_list args; __va_start(&args, msg); log(log_level::TRACE, msg, args); }
+	}
+} // namespace dimmer
