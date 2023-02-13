81f2bc5d2de87cd08b1ef1a964b197137e4c6003
Linus Vogel <44703775+linvogel@users.noreply.github.com>
2023-02-13 17:28:42 +0100

    compiles and runs on target

--- deploy.ps1
+++ deploy.ps1
@@ -7,8 +7,9 @@ New-Item -Force -ItemType "directory" $BUILD_DIR
 if ([System.IO.File]::Exists($BUILD_DIR)) { Remove-Item -Force $BUILD_DIR\source.tar }
 tar.exe -c -f $BUILD_DIR\source.tar .\glfw .\src .\CMakeLists.txt
 scp.exe $BUILD_DIR\source.tar dimmer:/firmware/source.tar
+ssh dimmer rm -rf /firmware/bin
 ssh dimmer mkdir -p /firmware/src
 ssh dimmer mkdir -p /firmware/bin
 ssh dimmer bsdtar -xvf source.tar -C src
-ssh dimmer cmake -S src -B bin
-ssh dimmer make -C bin firmware
+ssh dimmer cmake -S src -B bin -DTARGET=raspi
+ssh dimmer make -j 4 -C bin firmware
