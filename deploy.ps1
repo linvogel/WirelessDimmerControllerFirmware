fabda2613b43af5d62c5ebeedb834150ca922550
Linus Vogel <44703775+linvogel@users.noreply.github.com>
2023-02-13 02:16:52 +0100

    merge necessary

---
+++ deploy.ps1
@@ -0,0 +1,14 @@
+
+# Setup
+Set-Variable -Name BUILD_DIR -Value .\bin
+
+# sync to target
+New-Item -Force -ItemType "directory" $BUILD_DIR
+if ([System.IO.File]::Exists($BUILD_DIR)) { Remove-Item -Force $BUILD_DIR\source.tar }
+tar.exe -c -f $BUILD_DIR\source.tar .\glfw .\src .\CMakeLists.txt
+scp.exe $BUILD_DIR\source.tar dimmer:/firmware/source.tar
+ssh dimmer mkdir -p /firmware/src
+ssh dimmer mkdir -p /firmware/bin
+ssh dimmer bsdtar -xvf source.tar -C src
+ssh dimmer cmake -S src -B bin
+ssh dimmer make -C bin firmware
